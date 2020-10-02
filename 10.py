14ca026bfd108c7df50e02b2e7213a8e97868505
Santos Gallegos <stsewd@protonmail.com>
2020-10-02 13:17:01 -0500

    Solve problem 10

--- 10.py
+++ 10.py
@@ -1,6 +1,13 @@
-# FIXME: Check what number to add exactly when up is True or False
-
 import math
+
+def get_top(n):
+    i = 1
+    r = 0
+    while i <= n:
+        i += 2
+        r += 1
+    return r
+
 
 def triangles(n):
     if not n:
@@ -14,15 +21,20 @@ def triangles(n):
     up = False
     all_cols = [1, 3]
     for _ in range(3, n + 1):
+        col_up = all_cols[0]
+        col_down = all_cols[-1]
         for i in range(len(all_cols)):
-            if all_cols[i] % 2 == 1:
-                all_cols[i] += 2
-            else:
-                all_cols[i] += 3
-    if up:
-        all_cols.insert(0, math.ceil(all_cols[0]/2))
-        up = False
-    else:
-        all_cols.append(math.ceil(all_cols[-1]/2))
-        up = True
+            all_cols[i] += 2
+
+        if up:
+            all_cols.insert(0, get_top(col_up))
+            all_cols[-1] += col_down - 1
+            up = False
+        else:
+            all_cols.append(get_top(col_down))
+            all_cols[0] += col_up - 1
+            up = True
     return sum(all_cols)
+
+n = int(input())
+print(triangles(n))
